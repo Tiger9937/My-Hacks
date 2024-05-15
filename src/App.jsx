e1c6ad3dbf5ceb993c46194de7eb348f37c42d4b
jagannath sahoo <jagannathsahoo367900@gmail.coom>
2024-05-15 06:59:07 +0530

    app change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link} from 'react-router-dom';
+import InputBar from './components/common/inputBar'
+import Button from './components/common/BUTTON'
 
 const App = () => {
    return (
@@ -14,19 +16,20 @@ const App = () => {
 
       </div>
       <div className="flex space-x-4">
-        <Link to={"/singup"} className="px-4 py-2 bg-orange-600 rounded hover:bg-orange-700">Get Started</Link>
-        <Link to={"/singup"} className="px-4 py-2 bg-gray-700 rounded hover:bg-gray-600">Signin</Link>
+        
+        <Link to={"/singup"} className="px-4 py-2 bg-custom_Orang  rounded hover:bg-yellow-500 ">Get Started</Link>
+        <Link to={"/login"} className="px-4 py-2 bg-custom_Gray rounded hover:bg-gray-600">login</Link>
       </div>
     </nav>
 
 
     <div className="flex flex-col items-center mt-16">
       <div className="flex space-x-4 mb-8">
-        <button className="px-6 py-2 bg-orange-600 rounded hover:bg-orange-700">Hackathon</button>
-        <button className="px-6 py-2 bg-gray-700 rounded hover:bg-gray-600">Teams</button>
+        <Button className="px-6 py-2 bg-orange-600 rounded hover:bg-orange-700">Hackathon</Button>
+        <Button className="px-6 py-2 bg-gray-700 rounded hover:bg-gray-600">Teams</Button>
       </div>
       <div className="relative w-1/2">
-        <input type="text" placeholder="Search" className="w-full px-4 py-2 text-black rounded" />
+        <InputBar type={"text"} placeholder={"Search"} className={"w-full px-4 py-2 text-black rounded"} />
         <div className="absolute top-0 right-0 mt-2 mr-2">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 10-14 0 7 7 0 0014 0z" />
